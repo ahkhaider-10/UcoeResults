b932be4bfb3b9c2a48e05cea98676bd0bd9ddecc
tanveer-hash <tanveerhash7@gmail.com>
2023-09-16 15:38:33 +0530

    another test

--- main.js
+++ main.js
@@ -56,27 +56,27 @@ function get_result(resultid, rollno){
     };
 
     const queryString = new URLSearchParams(queryParams).toString();
-    const apiUrl = `${baseUrl}?${queryString}`;
+    const apiUrl = `https://thingproxy.freeboard.io/fetch/${baseUrl}?${queryString}`;
 
-    const headers = {
+    headers = {
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'Accept-Encoding': 'gzip, deflate, br',
         'Accept-Language': 'en-US,en;q=0.9',
         'Cache-Control': 'max-age=0',
-        'Connection': 'keep-alive',
+        'Sec-Ch-Ua': '"Chromium";v="116", "Not)A;Brand";v="24", "Google Chrome";v="116"',
+        'Sec-Ch-Ua-Mobile': '?0',
+        'Sec-Ch-Ua-Platform': '"Windows"',
         'Sec-Fetch-Dest': 'document',
         'Sec-Fetch-Mode': 'navigate',
         'Sec-Fetch-Site': 'none',
         'Sec-Fetch-User': '?1',
         'Upgrade-Insecure-Requests': '1',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
-        'sec-ch-ua': '"Chromium";v="116", "Not)A;Brand";v="24", "Google Chrome";v="116"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
-    };
+    }
 
     fetch(apiUrl, {
     method: 'GET',
-    headers: headers
+    headers : headers
     })
     .then(response => {
         if (response.ok) {
@@ -95,4 +95,4 @@ function get_result(resultid, rollno){
     });
 }
 
-get_result(resultDetails["cse"][0], 12004019);
+get_result(resultDetails["cse"][0], 12004002);
